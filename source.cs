38d16e63579ed71e2283e41fa6d5d41cf35aed34
s5689 <lemirl0ug@gmail.com>
2022-10-06 09:28:52 -0400

    Initial commit

---
+++ source.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace snake
+{
+    public class program
+    {
+        public void uglyBox()
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("|                         |");
+            Console.WriteLine("|                         |");
+            Console.WriteLine("|                         |");
+            Console.WriteLine("|                         |");
+            Console.WriteLine("|                         |");
+            Console.WriteLine("|                         |");
+            Console.WriteLine("|                         |");
+            Console.WriteLine("|                         |");
+            Console.WriteLine("|                         |");
+            Console.WriteLine("|                         |");
+            Console.WriteLine("---------------------------");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
